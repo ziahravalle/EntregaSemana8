cc437e10d8bd067b7a907b63d6dbb6a366059608
Edwin Flores <ejuniorfloress@gmail.com>
2023-09-27 10:34:01 -0500

    Todas las vistas de Admin
    listar, agregar, modificar, eliminar

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntregaSemana8.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult vistaClientes() { 
+            return View();
+        }
+        public IActionResult vistaProductos()
+        {
+            return View();
+        }
+        public IActionResult vistaProveedores()
+        {
+            return View();
+        }
+        public IActionResult vistaTrabajadores()
+        {
+            return View();
+        }
+
+        //clientes productos proveedores trabajadores
+    }
+}
