e67b2220664907143f9d51f863e20e3dcef1175d
Edwin Flores <ejuniorfloress@gmail.com>
2023-09-26 22:46:18 -0500

    El commit más grande de la vida
    MEDIO TRABAJO HECHO

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntregaSemana8.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult VerProducto()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,5 @@
+/*!
+* Start Bootstrap - Shop Item v5.0.6 (https://startbootstrap.com/template/shop-item)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
+*/
